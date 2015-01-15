94700eab3531036cbde69a7e9e1d671a7158a2ab
Schezuk <schezuk@163.com>
2015-01-16 03:11:03 +0800

    Update ver 0.1.0

---
+++ php/libConfig.php
@@ -0,0 +1,9 @@
+<?php
+	/* database const */
+	$DB_HOST="localhost";	//database address
+	$DB_USER="UserName";	//database username
+	$DB_PSWD="PassWord";	//database password
+	$DB_NAME="NameOfDB";	//databese name
+	$DB_PORT=      3306;	//database port
+	/* be aware 'const' works ONLY INSIDE of a class definition */
+?>
